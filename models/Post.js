7ef269bbd151b245a7c29e6c16bfacde74212699
LisaTLe <lisa.le1127@gmail.com>
2022-08-27 20:38:21 -0600

    Model post updated

---
+++ models/Post.js
@@ -0,0 +1,16 @@
+const { Model, Datatypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    title: Datatypes.STRING,
+    body: Datatypes.STRING,
+  },
+  {
+    sequelize,
+  }
+);
+
+module.exports = Post;
