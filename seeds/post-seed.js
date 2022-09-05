ab325e8afb57d57f69ab28d86b8198ab641a7abb
LisaTLe <lisa.le1127@gmail.com>
2022-09-05 08:22:40 -0600

    edit seed files

---
+++ seeds/post-seed.js
@@ -0,0 +1,15 @@
+const {Post} = require("../models");
+
+const postData = [
+    {
+        title: "Tech Blog is Finally Here!",
+        post_content: "This blog is a place where you can post all kinds of cool facts and finds about technology!",
+        user_id: 1
+    }
+]
+
+const seedPost = async => {
+    await Post.bulkCreate(postData);
+};
+
+module.exports = seedPost;
---
+++ seeds/user-seed.js
@@ -0,0 +1,15 @@
+const {User} = require("../models");
+
+const userData = [
+    {
+        username: "test_123",
+        password: "test123",
+        github: "test123"
+    }
+]
+
+const seedUser = async => {
+    await User.bulkCreate(userData);
+};
+
+module.exports = seedUser;
