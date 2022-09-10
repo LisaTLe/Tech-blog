6a1294efdcdc6484131e057327e5256d18ee8e28
LisaTLe <lisa.le1127@gmail.com>
2022-09-09 20:37:33 -0600

    finish comment model

--- models/Comment.js
+++ models/Comment.js
@@ -5,13 +5,41 @@ class Comment extends Model {}
 
 Comment.init(
   {
-    body: {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+    comment_text: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [1],
+      },
     },
   },
   {
     sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
   }
 );
 
